aa7135602d2319fa76db660545414bcb50c5a187
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2023-01-13 18:25:42 +0600

    Added sample code for sorting map by keySet and Values

---
+++ src/datastructure/stream/sorting/MapSorting.java
@@ -0,0 +1,47 @@
+package datastructure.stream.sorting;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MapSorting {
+
+    public static void main(String[] args) {
+        Map<Integer, Integer> map = getMap();
+
+        System.out.println(sortMapByKeys(map));
+        System.out.println(sortMapByValues(map));
+    }
+
+    private static Map<Integer, Integer> sortMapByKeys(Map<Integer, Integer> map) {
+//        return new TreeMap<>(map);
+        return map.entrySet()
+                .stream()
+//                .sorted(Map.Entry.comparingByKey())
+                .sorted(Comparator.comparing(Map.Entry::getKey)) // Sorted by default ascending order
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    private static Map<Integer, Integer> sortMapByValues(Map<Integer, Integer> map) {
+        return map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Collections.reverseOrder())) // Sorted by descending order
+//                .sorted(Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()))
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+    }
+
+    private static Map<Integer, Integer> getMap() {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(1, 101);
+        map.put(2, 103);
+        map.put(3, 102);
+        map.put(4, 100);
+
+        return map;
+    }
+}
