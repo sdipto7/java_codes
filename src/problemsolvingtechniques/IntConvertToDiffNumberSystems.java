42703eb09326dd4dd279a2a516a0b8eaccd11c7e
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2023-01-20 15:03:21 +0600

    Added different techniques to convert decimal number to binary number

--- src/problemsolvingtechniques/IntConvertToDiffNumberSystems.java
+++ src/problemsolvingtechniques/IntConvertToDiffNumberSystems.java
@@ -3,22 +3,73 @@ package problemsolvingtechniques;
 public class IntConvertToDiffNumberSystems {
 
     public static void main(String[] args) {
-        int num = 16;
+        int positiveNum = 16;
+        int negativeNum = -16;
 
-        System.out.println(String.format("Binary Representation of Integer %d is: %s", num, getBinaryRepresentation(num)));
-        System.out.println(String.format("Octal Representation of Integer %d is: %s", num, getOctalRepresentation(num)));
-        System.out.println(String.format("Hexadecimal Representation of Integer %d is: %s", num, getHexadecimalRepresentation(num)));
+        System.out.println(String.format("Octal Representation of Positive Integer %d using builtin method is: %s",
+                positiveNum, getOctalRepresentationByIntegerClass(positiveNum)));
+
+        System.out.println(String.format("Hexadecimal Representation of Positive Integer %d using builtin method is: %s",
+                positiveNum, getHexadecimalRepresentationByIntegerClass(positiveNum)));
+
+        System.out.println(String.format("Binary Representation of Positive Integer %d using builtin method is: %s",
+                positiveNum, getBinaryRepresentationByIntegerClass(positiveNum)));
+
+        System.out.println(String.format("Binary Representation of Positive Integer %d is: %s",
+                positiveNum, convertDecimalToBinary(positiveNum)));
+
+        System.out.println(String.format("Binary Representation of Positive Integer using bit shifting technique %d is: %s",
+                positiveNum, convertDecimalToBinaryUsingBitShiftingOperators(positiveNum)));
+
+        System.out.println();
+
+        System.out.println(String.format("Octal Representation of Negative Integer %d using builtin method is: %s",
+                positiveNum, getOctalRepresentationByIntegerClass(negativeNum)));
+
+        System.out.println(String.format("Hexadecimal Representation of Negative Integer %d using builtin method is: %s",
+                positiveNum, getHexadecimalRepresentationByIntegerClass(negativeNum)));
+
+        System.out.println(String.format("Binary Representation of Negative Integer %d using builtin method is: %s",
+                positiveNum, getBinaryRepresentationByIntegerClass(negativeNum)));
+
+        System.out.println(String.format("Binary Representation of Negative Integer %d is: %s",
+                positiveNum, convertDecimalToBinary(negativeNum)));
+
+        System.out.println(String.format("Binary Representation of Negative Integer using bit shifting technique %d is: %s",
+                positiveNum, convertDecimalToBinaryUsingBitShiftingOperators(negativeNum)));
     }
 
-    private static String getBinaryRepresentation(int num) {
+    private static String getBinaryRepresentationByIntegerClass(int num) {
         return Integer.toBinaryString(num);
     }
 
-    private static String getOctalRepresentation(int num) {
+    /* This technique will work only for positive decimal number*/
+    private static String convertDecimalToBinary(int num) {
+        StringBuilder binaryNumber = new StringBuilder(); // We can also use array with length 32 to store each bit value of 32-bit integer
+        while (num > 0) {
+            binaryNumber.append(num % 2);
+            num = num / 2;
+        }
+
+        return binaryNumber.reverse().toString();
+    }
+
+    /* This bit shifting technique will work for both positive and negative decimal number*/
+    private static String convertDecimalToBinaryUsingBitShiftingOperators(int num) {
+        StringBuilder binaryNumber = new StringBuilder(); // We can also use array with length 32 to store each bit value of 32-bit integer
+        for (int bit = 31; bit >= 0; --bit) {
+            int bitValue = ((num >> bit) & 1) == 1 ? 1 : 0;
+            binaryNumber.append(bitValue);
+        }
+
+        return binaryNumber.toString();
+    }
+
+    private static String getOctalRepresentationByIntegerClass(int num) {
         return Integer.toOctalString(num);
     }
 
-    private static String getHexadecimalRepresentation(int num) {
+    private static String getHexadecimalRepresentationByIntegerClass(int num) {
         return Integer.toHexString(num);
     }
 }
